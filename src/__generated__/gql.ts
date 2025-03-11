7f9fc731517d5d001da77944777a48caa6d9bd93
Matthew Rider <mattwr18@gmail.com>
2025-03-11 11:46:09 +0100

    Format

--- src/__generated__/gql.ts
+++ src/__generated__/gql.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
-import * as types from './graphql';
-import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+import * as types from './graphql'
+import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core'
 
 /**
  * Map of all GraphQL operations in the project.
@@ -14,15 +14,18 @@ import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-
  * Learn more about it here: https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#reducing-bundle-size
  */
 type Documents = {
-    "\n  mutation VoteOnQuestion($questionId: ID!, $choiceId: ID!) {\n    vote(questionId: $questionId, choiceId: $choiceId) {\n      question {\n        id\n        questionText\n        choices {\n          id\n          choiceText\n          votes\n        }\n      }\n    }\n  }\n": typeof types.VoteOnQuestionDocument,
-    "\n  query FetchQuestions {\n    fetchQuestions {\n      id\n      questionText\n    }\n  }\n": typeof types.FetchQuestionsDocument,
-    "\n  query FetchQuestion($id: ID!) {\n    fetchQuestion(id: $id) {\n      id\n      questionText\n      choices {\n        id\n        choiceText\n        votes\n      }\n    }\n  }\n": typeof types.FetchQuestionDocument,
-};
+  '\n  mutation VoteOnQuestion($questionId: ID!, $choiceId: ID!) {\n    vote(questionId: $questionId, choiceId: $choiceId) {\n      question {\n        id\n        questionText\n        choices {\n          id\n          choiceText\n          votes\n        }\n      }\n    }\n  }\n': typeof types.VoteOnQuestionDocument
+  '\n  query FetchQuestions {\n    fetchQuestions {\n      id\n      questionText\n    }\n  }\n': typeof types.FetchQuestionsDocument
+  '\n  query FetchQuestion($id: ID!) {\n    fetchQuestion(id: $id) {\n      id\n      questionText\n      choices {\n        id\n        choiceText\n        votes\n      }\n    }\n  }\n': typeof types.FetchQuestionDocument
+}
 const documents: Documents = {
-    "\n  mutation VoteOnQuestion($questionId: ID!, $choiceId: ID!) {\n    vote(questionId: $questionId, choiceId: $choiceId) {\n      question {\n        id\n        questionText\n        choices {\n          id\n          choiceText\n          votes\n        }\n      }\n    }\n  }\n": types.VoteOnQuestionDocument,
-    "\n  query FetchQuestions {\n    fetchQuestions {\n      id\n      questionText\n    }\n  }\n": types.FetchQuestionsDocument,
-    "\n  query FetchQuestion($id: ID!) {\n    fetchQuestion(id: $id) {\n      id\n      questionText\n      choices {\n        id\n        choiceText\n        votes\n      }\n    }\n  }\n": types.FetchQuestionDocument,
-};
+  '\n  mutation VoteOnQuestion($questionId: ID!, $choiceId: ID!) {\n    vote(questionId: $questionId, choiceId: $choiceId) {\n      question {\n        id\n        questionText\n        choices {\n          id\n          choiceText\n          votes\n        }\n      }\n    }\n  }\n':
+    types.VoteOnQuestionDocument,
+  '\n  query FetchQuestions {\n    fetchQuestions {\n      id\n      questionText\n    }\n  }\n':
+    types.FetchQuestionsDocument,
+  '\n  query FetchQuestion($id: ID!) {\n    fetchQuestion(id: $id) {\n      id\n      questionText\n      choices {\n        id\n        choiceText\n        votes\n      }\n    }\n  }\n':
+    types.FetchQuestionDocument,
+}
 
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
@@ -36,23 +39,30 @@ const documents: Documents = {
  * The query argument is unknown!
  * Please regenerate the types.
  */
-export function gql(source: string): unknown;
+export function gql(source: string): unknown
 
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(source: "\n  mutation VoteOnQuestion($questionId: ID!, $choiceId: ID!) {\n    vote(questionId: $questionId, choiceId: $choiceId) {\n      question {\n        id\n        questionText\n        choices {\n          id\n          choiceText\n          votes\n        }\n      }\n    }\n  }\n"): (typeof documents)["\n  mutation VoteOnQuestion($questionId: ID!, $choiceId: ID!) {\n    vote(questionId: $questionId, choiceId: $choiceId) {\n      question {\n        id\n        questionText\n        choices {\n          id\n          choiceText\n          votes\n        }\n      }\n    }\n  }\n"];
+export function gql(
+  source: '\n  mutation VoteOnQuestion($questionId: ID!, $choiceId: ID!) {\n    vote(questionId: $questionId, choiceId: $choiceId) {\n      question {\n        id\n        questionText\n        choices {\n          id\n          choiceText\n          votes\n        }\n      }\n    }\n  }\n',
+): (typeof documents)['\n  mutation VoteOnQuestion($questionId: ID!, $choiceId: ID!) {\n    vote(questionId: $questionId, choiceId: $choiceId) {\n      question {\n        id\n        questionText\n        choices {\n          id\n          choiceText\n          votes\n        }\n      }\n    }\n  }\n']
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(source: "\n  query FetchQuestions {\n    fetchQuestions {\n      id\n      questionText\n    }\n  }\n"): (typeof documents)["\n  query FetchQuestions {\n    fetchQuestions {\n      id\n      questionText\n    }\n  }\n"];
+export function gql(
+  source: '\n  query FetchQuestions {\n    fetchQuestions {\n      id\n      questionText\n    }\n  }\n',
+): (typeof documents)['\n  query FetchQuestions {\n    fetchQuestions {\n      id\n      questionText\n    }\n  }\n']
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(source: "\n  query FetchQuestion($id: ID!) {\n    fetchQuestion(id: $id) {\n      id\n      questionText\n      choices {\n        id\n        choiceText\n        votes\n      }\n    }\n  }\n"): (typeof documents)["\n  query FetchQuestion($id: ID!) {\n    fetchQuestion(id: $id) {\n      id\n      questionText\n      choices {\n        id\n        choiceText\n        votes\n      }\n    }\n  }\n"];
+export function gql(
+  source: '\n  query FetchQuestion($id: ID!) {\n    fetchQuestion(id: $id) {\n      id\n      questionText\n      choices {\n        id\n        choiceText\n        votes\n      }\n    }\n  }\n',
+): (typeof documents)['\n  query FetchQuestion($id: ID!) {\n    fetchQuestion(id: $id) {\n      id\n      questionText\n      choices {\n        id\n        choiceText\n        votes\n      }\n    }\n  }\n']
 
 export function gql(source: string) {
-  return (documents as any)[source] ?? {};
+  return (documents as any)[source] ?? {}
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
+  TDocumentNode extends DocumentNode<infer TType, any> ? TType : never
--- src/__generated__/graphql.ts
+++ src/__generated__/graphql.ts
@@ -1,92 +1,271 @@
 /* eslint-disable */
-import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
-export type Maybe<T> = T | null;
-export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
-export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core'
+export type Maybe<T> = T | null
+export type InputMaybe<T> = Maybe<T>
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] }
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> }
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> }
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = {
+  [_ in K]?: never
+}
+export type Incremental<T> =
+  | T
+  | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never }
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: { input: string; output: string; }
-  String: { input: string; output: string; }
-  Boolean: { input: boolean; output: boolean; }
-  Int: { input: number; output: number; }
-  Float: { input: number; output: number; }
+  ID: { input: string; output: string }
+  String: { input: string; output: string }
+  Boolean: { input: boolean; output: boolean }
+  Int: { input: number; output: number }
+  Float: { input: number; output: number }
   /**
    * The `DateTime` scalar type represents a DateTime
    * value as specified by
    * [iso8601](https://en.wikipedia.org/wiki/ISO_8601).
    */
-  DateTime: { input: any; output: any; }
-};
+  DateTime: { input: any; output: any }
+}
 
 export type ChoiceType = {
-  __typename?: 'ChoiceType';
-  choiceText: Scalars['String']['output'];
-  id: Scalars['ID']['output'];
-  question: QuestionType;
-  votes: Scalars['Int']['output'];
-};
+  __typename?: 'ChoiceType'
+  choiceText: Scalars['String']['output']
+  id: Scalars['ID']['output']
+  question: QuestionType
+  votes: Scalars['Int']['output']
+}
 
 export type Mutation = {
-  __typename?: 'Mutation';
-  vote?: Maybe<VoteMutation>;
-};
-
+  __typename?: 'Mutation'
+  vote?: Maybe<VoteMutation>
+}
 
 export type MutationVoteArgs = {
-  choiceId: Scalars['ID']['input'];
-  questionId: Scalars['ID']['input'];
-};
+  choiceId: Scalars['ID']['input']
+  questionId: Scalars['ID']['input']
+}
 
 export type Query = {
-  __typename?: 'Query';
-  fetchQuestion?: Maybe<QuestionType>;
-  fetchQuestions?: Maybe<Array<Maybe<QuestionType>>>;
-};
-
+  __typename?: 'Query'
+  fetchQuestion?: Maybe<QuestionType>
+  fetchQuestions?: Maybe<Array<Maybe<QuestionType>>>
+}
 
 export type QueryFetchQuestionArgs = {
-  id: Scalars['ID']['input'];
-};
+  id: Scalars['ID']['input']
+}
 
 export type QuestionType = {
-  __typename?: 'QuestionType';
-  choices: Array<ChoiceType>;
-  id: Scalars['ID']['output'];
-  pubDate: Scalars['DateTime']['output'];
-  questionText: Scalars['String']['output'];
-};
+  __typename?: 'QuestionType'
+  choices: Array<ChoiceType>
+  id: Scalars['ID']['output']
+  pubDate: Scalars['DateTime']['output']
+  questionText: Scalars['String']['output']
+}
 
 export type VoteMutation = {
-  __typename?: 'VoteMutation';
-  choice?: Maybe<ChoiceType>;
-  question?: Maybe<QuestionType>;
-};
+  __typename?: 'VoteMutation'
+  choice?: Maybe<ChoiceType>
+  question?: Maybe<QuestionType>
+}
 
 export type VoteOnQuestionMutationVariables = Exact<{
-  questionId: Scalars['ID']['input'];
-  choiceId: Scalars['ID']['input'];
-}>;
+  questionId: Scalars['ID']['input']
+  choiceId: Scalars['ID']['input']
+}>
 
+export type VoteOnQuestionMutation = {
+  __typename?: 'Mutation'
+  vote?: {
+    __typename?: 'VoteMutation'
+    question?: {
+      __typename?: 'QuestionType'
+      id: string
+      questionText: string
+      choices: Array<{ __typename?: 'ChoiceType'; id: string; choiceText: string; votes: number }>
+    } | null
+  } | null
+}
 
-export type VoteOnQuestionMutation = { __typename?: 'Mutation', vote?: { __typename?: 'VoteMutation', question?: { __typename?: 'QuestionType', id: string, questionText: string, choices: Array<{ __typename?: 'ChoiceType', id: string, choiceText: string, votes: number }> } | null } | null };
+export type FetchQuestionsQueryVariables = Exact<{ [key: string]: never }>
 
-export type FetchQuestionsQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-export type FetchQuestionsQuery = { __typename?: 'Query', fetchQuestions?: Array<{ __typename?: 'QuestionType', id: string, questionText: string } | null> | null };
+export type FetchQuestionsQuery = {
+  __typename?: 'Query'
+  fetchQuestions?: Array<{
+    __typename?: 'QuestionType'
+    id: string
+    questionText: string
+  } | null> | null
+}
 
 export type FetchQuestionQueryVariables = Exact<{
-  id: Scalars['ID']['input'];
-}>;
+  id: Scalars['ID']['input']
+}>
 
+export type FetchQuestionQuery = {
+  __typename?: 'Query'
+  fetchQuestion?: {
+    __typename?: 'QuestionType'
+    id: string
+    questionText: string
+    choices: Array<{ __typename?: 'ChoiceType'; id: string; choiceText: string; votes: number }>
+  } | null
+}
 
-export type FetchQuestionQuery = { __typename?: 'Query', fetchQuestion?: { __typename?: 'QuestionType', id: string, questionText: string, choices: Array<{ __typename?: 'ChoiceType', id: string, choiceText: string, votes: number }> } | null };
-
-
-export const VoteOnQuestionDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"VoteOnQuestion"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"questionId"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"ID"}}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"choiceId"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"ID"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"vote"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"questionId"},"value":{"kind":"Variable","name":{"kind":"Name","value":"questionId"}}},{"kind":"Argument","name":{"kind":"Name","value":"choiceId"},"value":{"kind":"Variable","name":{"kind":"Name","value":"choiceId"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"question"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"questionText"}},{"kind":"Field","name":{"kind":"Name","value":"choices"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"choiceText"}},{"kind":"Field","name":{"kind":"Name","value":"votes"}}]}}]}}]}}]}}]} as unknown as DocumentNode<VoteOnQuestionMutation, VoteOnQuestionMutationVariables>;
-export const FetchQuestionsDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"FetchQuestions"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"fetchQuestions"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"questionText"}}]}}]}}]} as unknown as DocumentNode<FetchQuestionsQuery, FetchQuestionsQueryVariables>;
-export const FetchQuestionDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"FetchQuestion"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"id"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"ID"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"fetchQuestion"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"id"},"value":{"kind":"Variable","name":{"kind":"Name","value":"id"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"questionText"}},{"kind":"Field","name":{"kind":"Name","value":"choices"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"choiceText"}},{"kind":"Field","name":{"kind":"Name","value":"votes"}}]}}]}}]}}]} as unknown as DocumentNode<FetchQuestionQuery, FetchQuestionQueryVariables>;
+export const VoteOnQuestionDocument = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'mutation',
+      name: { kind: 'Name', value: 'VoteOnQuestion' },
+      variableDefinitions: [
+        {
+          kind: 'VariableDefinition',
+          variable: { kind: 'Variable', name: { kind: 'Name', value: 'questionId' } },
+          type: {
+            kind: 'NonNullType',
+            type: { kind: 'NamedType', name: { kind: 'Name', value: 'ID' } },
+          },
+        },
+        {
+          kind: 'VariableDefinition',
+          variable: { kind: 'Variable', name: { kind: 'Name', value: 'choiceId' } },
+          type: {
+            kind: 'NonNullType',
+            type: { kind: 'NamedType', name: { kind: 'Name', value: 'ID' } },
+          },
+        },
+      ],
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'vote' },
+            arguments: [
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'questionId' },
+                value: { kind: 'Variable', name: { kind: 'Name', value: 'questionId' } },
+              },
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'choiceId' },
+                value: { kind: 'Variable', name: { kind: 'Name', value: 'choiceId' } },
+              },
+            ],
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'question' },
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                      { kind: 'Field', name: { kind: 'Name', value: 'questionText' } },
+                      {
+                        kind: 'Field',
+                        name: { kind: 'Name', value: 'choices' },
+                        selectionSet: {
+                          kind: 'SelectionSet',
+                          selections: [
+                            { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                            { kind: 'Field', name: { kind: 'Name', value: 'choiceText' } },
+                            { kind: 'Field', name: { kind: 'Name', value: 'votes' } },
+                          ],
+                        },
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+} as unknown as DocumentNode<VoteOnQuestionMutation, VoteOnQuestionMutationVariables>
+export const FetchQuestionsDocument = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'query',
+      name: { kind: 'Name', value: 'FetchQuestions' },
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'fetchQuestions' },
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'questionText' } },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+} as unknown as DocumentNode<FetchQuestionsQuery, FetchQuestionsQueryVariables>
+export const FetchQuestionDocument = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'query',
+      name: { kind: 'Name', value: 'FetchQuestion' },
+      variableDefinitions: [
+        {
+          kind: 'VariableDefinition',
+          variable: { kind: 'Variable', name: { kind: 'Name', value: 'id' } },
+          type: {
+            kind: 'NonNullType',
+            type: { kind: 'NamedType', name: { kind: 'Name', value: 'ID' } },
+          },
+        },
+      ],
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'fetchQuestion' },
+            arguments: [
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'id' },
+                value: { kind: 'Variable', name: { kind: 'Name', value: 'id' } },
+              },
+            ],
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'questionText' } },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'choices' },
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                      { kind: 'Field', name: { kind: 'Name', value: 'choiceText' } },
+                      { kind: 'Field', name: { kind: 'Name', value: 'votes' } },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+} as unknown as DocumentNode<FetchQuestionQuery, FetchQuestionQueryVariables>
